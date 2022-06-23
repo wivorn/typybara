8e41afbe06642de0c4d95acec4c48bf00b42a41e
Will <wivorn.c@gmail.com>
2022-06-23 15:02:34 -0400

    add typing and error checking

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
-let mainText = "Hello world, my name is Capybara"
+class App {
+  constructor() {
+    this.mainText = "Hello world, my name is Capybara"
+    this.typedText = []
+    document.addEventListener("keydown", this.handleKeyDown.bind(this))
 
-const mainTextDiv = document.getElementById("main-text")
+    this.mainTextDiv = document.getElementById("main-text")
+    this.typedTextDiv = document.getElementById("typed-text")
 
-function setMainText(text) {
-  mainTextDiv.innerHTML = text
+    this.mainTextDiv.innerHTML = this.mainText
+  }
+
+  handleKeyDown(char) {
+    const key = char.key
+    const regex = new RegExp("^[a-zA-Z \,\.\"\'\?]$")
+    if (key === "Backspace") {
+      this.typedText.pop()
+      this.renderTypedText()
+    } else if (key.match(regex)) {
+      this.typedText.push(key)
+      this.renderTypedText()
+    }
+  }
+
+  // return boolean for each matched typed character to main text character
+  textDiff(mainText, typedText) {
+    return typedText.map((char, index) => char === mainText[index] ? true : false)
+  }
+
+  renderTypedText() {
+    const diff = this.textDiff(this.mainText, this.typedText)
+    const renderedHtml = this.typedText.map((char, index) => {
+      const color = diff[index] ? "" : "bg-red-100"
+      const letter = char === " " ? "&nbsp;" : char
+      return `<span class="${color}">${letter}</span>`
+    }).join('')
+
+    this.typedTextDiv.innerHTML = renderedHtml
+  }
 }
 
-setMainText(mainText)
+const app = new App()
